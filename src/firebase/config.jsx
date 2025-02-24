7795e9deb90ba985553fcf55c011d1a02e260de9
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-02-24 17:16:41 -0300

    525-545-635: News Cards.

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -9,34 +9,34 @@ const environment = window.location.hostname
 
 if (environment.includes('gwi-news')) {
     firebaseConfig = {
-        apiKey: import.meta.env.REACT_APP_PROD_API_KEY,
-        authDomain: import.meta.env.REACT_APP_PROD_AUTH_DOMAIN,
-        projectId: import.meta.env.REACT_APP_PROD_PROJECT_ID,
-        storageBucket: import.meta.env.REACT_APP_PROD_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
-        appId: import.meta.env.REACT_APP_PROD_APP_ID,
-        measurementId: import.meta.env.REACT_APP_PROD_MEASUREMENT_ID
+        apiKey: import.meta.env.VITE_PROD_API_KEY,
+        authDomain: import.meta.env.VITE_PROD_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_PROD_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_PROD_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_PROD_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_PROD_APP_ID,
+        measurementId: import.meta.env.VITE_PROD_MEASUREMENT_ID
     }
 } else if (environment.includes('test')) {
     firebaseConfig = {
-        apiKey: import.meta.env.REACT_APP_TEST_API_KEY,
-        authDomain: import.meta.env.REACT_APP_TEST_AUTH_DOMAIN,
-        databaseUrl: import.meta.env.REACT_APP_TEST_DATABASE_URL,
-        projectId: import.meta.env.REACT_APP_TEST_PROJECT_ID,
-        storageBucket: import.meta.env.REACT_APP_TEST_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env.REACT_APP_TEST_MESSAGING_SENDER_ID,
-        appId: import.meta.env.REACT_APP_TEST_APP_ID,
-        measurementId: import.meta.env.REACT_APP_TEST_MEASUREMENT_ID
+        apiKey: import.meta.env.VITE_TEST_API_KEY,
+        authDomain: import.meta.env.VITE_TEST_AUTH_DOMAIN,
+        databaseUrl: import.meta.env.VITE_TEST_DATABASE_URL,
+        projectId: import.meta.env.VITE_TEST_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_TEST_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_TEST_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_TEST_APP_ID,
+        measurementId: import.meta.env.VITE_TEST_MEASUREMENT_ID
     }
 } else {
     firebaseConfig = {
-        apiKey: import.meta.env.REACT_APP_DEV_API_KEY,
-        authDomain: import.meta.env.REACT_APP_DEV_AUTH_DOMAIN,
-        projectId: import.meta.env.REACT_APP_DEV_PROJECT_ID,
-        storageBucket: import.meta.env.REACT_APP_DEV_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
-        appId: import.meta.env.REACT_APP_DEV_APP_ID,
-        measurementId: import.meta.env.REACT_APP_DEV_MEASUREMENT_ID
+        apiKey: import.meta.env.VITE_DEV_API_KEY,
+        authDomain: import.meta.env.VITE_DEV_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_DEV_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_DEV_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_DEV_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_DEV_APP_ID,
+        measurementId: import.meta.env.VITE_DEV_MEASUREMENT_ID
     }
 }
 
