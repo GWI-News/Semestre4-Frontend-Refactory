2f595f84347f3c5713b050766974b0c43c73fb91
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2025-02-26 10:14:07 -0300

    525-F-T: Secrets GCP para ambientes de deploy.

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -8,36 +8,11 @@ let firebaseConfig = {}
 const environment = window.location.hostname
 
 if (environment.includes('gwi-news')) {
-    firebaseConfig = {
-        apiKey: import.meta.env.VITE_PROD_API_KEY,
-        authDomain: import.meta.env.VITE_PROD_AUTH_DOMAIN,
-        projectId: import.meta.env.VITE_PROD_PROJECT_ID,
-        storageBucket: import.meta.env.VITE_PROD_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env.VITE_PROD_MESSAGING_SENDER_ID,
-        appId: import.meta.env.VITE_PROD_APP_ID,
-        measurementId: import.meta.env.VITE_PROD_MEASUREMENT_ID
-    }
+    firebaseConfig = JSON.parse(import.meta.env.VITE_PROD_ENVIRONMENT_API_KEYS);
 } else if (environment.includes('test')) {
-    firebaseConfig = {
-        apiKey: import.meta.env.VITE_TEST_API_KEY,
-        authDomain: import.meta.env.VITE_TEST_AUTH_DOMAIN,
-        databaseUrl: import.meta.env.VITE_TEST_DATABASE_URL,
-        projectId: import.meta.env.VITE_TEST_PROJECT_ID,
-        storageBucket: import.meta.env.VITE_TEST_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env.VITE_TEST_MESSAGING_SENDER_ID,
-        appId: import.meta.env.VITE_TEST_APP_ID,
-        measurementId: import.meta.env.VITE_TEST_MEASUREMENT_ID
-    }
+    firebaseConfig = JSON.parse(import.meta.env.VITE_TEST_ENVIRONMENT_API_KEYS);
 } else {
-    firebaseConfig = {
-        apiKey: import.meta.env.VITE_DEV_API_KEY,
-        authDomain: import.meta.env.VITE_DEV_AUTH_DOMAIN,
-        projectId: import.meta.env.VITE_DEV_PROJECT_ID,
-        storageBucket: import.meta.env.VITE_DEV_STORAGE_BUCKET,
-        messagingSenderId: import.meta.env.VITE_DEV_MESSAGING_SENDER_ID,
-        appId: import.meta.env.VITE_DEV_APP_ID,
-        measurementId: import.meta.env.VITE_DEV_MEASUREMENT_ID
-    }
+    firebaseConfig = JSON.parse(import.meta.env.VITE_DEV_ENVIRONMENT_API_KEYS);
 }
 
 const app = initializeApp(firebaseConfig)
